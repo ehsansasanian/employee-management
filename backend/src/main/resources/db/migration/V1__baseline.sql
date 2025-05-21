7f13e39d3e1b1848dc7b55ffd296a06bf280e101
Ehsan <ehsan.sasanian@gmail.com>
2025-05-21 23:23:54 +0200

    add sql baseline script

---
+++ backend/src/main/resources/db/migration/V1__baseline.sql
@@ -0,0 +1,19 @@
+CREATE TABLE departments
+(
+    id         BIGSERIAL PRIMARY KEY,
+    name       TEXT NOT NULL UNIQUE,
+    created_at TIMESTAMPTZ  NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ  NOT NULL DEFAULT now()
+);
+
+CREATE TABLE employees
+(
+    id            BIGSERIAL PRIMARY KEY,
+    firstname     TEXT NOT NULL,
+    lastname     TEXT NOT NULL,
+    address       TEXT,
+    phone         TEXT NOT NULL,
+    email         TEXT NOT NULL UNIQUE,
+    department_id BIGINT       NOT NULL REFERENCES departments (id),
+    CONSTRAINT chk_phone CHECK (phone ~ '^\+\d{7,15}$')
+);
